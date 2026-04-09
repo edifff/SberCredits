c7d0119f334616e2c64bc85070edf88572f25213
edifff <mishapronin2015@gmail.com>
2026-04-09 20:22:57 +0300

    Продолжена настройка защиты

--- SberSecurity/src/main/java/com/example/sberlending/config/SecurityConfig.java
+++ SberSecurity/src/main/java/com/example/sberlending/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.example.sberlending.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -16,24 +19,54 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws SecurityException{
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws SecurityException {
+        httpSecurity
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/perform-login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/login?error")
+                        .permitAll()
+                );
+        httpSecurity
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false)
+                        .expiredUrl("/login?expired")
+                );
+
         httpSecurity.httpBasic(Customizer.withDefaults());
-        httpSecurity.authorizeHttpRequests(c->c.anyRequest().permitAll());
+        httpSecurity.authorizeHttpRequests(auth ->auth
+                        .requestMatchers("/auth/**", "/login" ).permitAll()
+                        .anyRequest().authenticated());
+
+        httpSecurity.exceptionHandling(ex->ex
+                .authenticationEntryPoint((req, res, e) -> res.sendError(401))
+                .accessDeniedHandler((req, res, e) -> res.sendError(403)));
+
+        httpSecurity
+                .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/**")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+        );
         return httpSecurity.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user= User.withUsername("admin")
-                .password("admin")
-                .authorities("read")
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder, Environment env) {
+        UserDetails user = User
+                .withUsername(env.getProperty("USER_NAME"))
+                .password(passwordEncoder().encode(env.getProperty("USER_PASSWORD")))
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
